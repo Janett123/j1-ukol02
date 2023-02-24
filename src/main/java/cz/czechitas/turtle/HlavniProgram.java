95855805749f9b8e3c771d1086586e3a72b2023c
Janett123 <masiko@centrum.cz>
2023-02-24 10:00:15 +0100

    Nakreslená mašinka a nakreslený sněhulák

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -34,6 +34,24 @@ public class HlavniProgram {
         zofka.setLocation(780,380);
         nakresliKruh(4.3633);
 
+        //kreslení sněhuláka
+        zofka.setLocation(400,180);
+        nakresliKruh(5.2336);
+
+        zofka.setLocation(400,280);
+        nakresliKruh(8.7266);
+
+        zofka.setLocation(400,420);
+        nakresliKruh(12.2173);
+
+        zofka.setLocation(330,230);
+        nakresliKruh(3.4907);
+
+        zofka.setLocation(465,230);
+
+        nakresliKruh(3.4907);
+
+
     }
 
     private void nakresliKruh(double vysec) {
